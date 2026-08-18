18bb666e52afe49768b4771ba5635ab95bbced9f
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-18 19:27:53 +0300

    add health endPoint

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,22 @@ import dotenv from 'dotenv';
 //import { pool } from './db/index.ts';
 import {runMigration} from "./db/migrate";
 import {startRetentionScheduler} from "./services";
+import {pool} from "./db";
 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.get('/health', async (req, res ) => {
+    try {
+        await pool.query(' SELECT 1');
+        res.status(200).json({ status: 'healthy', database: 'connected' });
+    }catch (error) {
+        res.status(500).json({ status: 'unhealthy', database: 'disconnected'});
+    }
+});
 
 
 // app.listen(PORT, () => {
