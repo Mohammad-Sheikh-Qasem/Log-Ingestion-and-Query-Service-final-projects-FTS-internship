8cf651728480efecc407eee4097a49fb07b91998
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-18 19:20:14 +0300

    add a handler function to run the server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 //import { pool } from './db/index.ts';
 import {runMigration} from "./db/migrate";
+import {startRetentionScheduler} from "./services";
 
 
 dotenv.config();
@@ -10,15 +11,25 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
-// async function startServer(){
-//     try {
-//         await runMigration();
-//
-//     }catch(error){
-//
-//     }
-// }
+async function startServer(){
+    try {
+        await runMigration();
+
+        startRetentionScheduler();
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+
+    }catch(error){
+        console.error('Failed to start server', error);
+        process.exit(1);
+
+    }
+}
+
+startServer();
