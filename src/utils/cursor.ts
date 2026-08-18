6915d388dd2b0a3a10137e83edb58bd2280dedf6
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-18 19:40:19 +0300

    add decodeCursor function

--- src/utils/cursor.ts
+++ src/utils/cursor.ts
@@ -6,3 +6,17 @@ export interface CursorPayload {
 export function encodeCursor(payload: CursorPayload): string {
     return Buffer.from(JSON.stringify(payload)).toString('base64');
 }
+
+export function decodeCursor(cursorStr: string): CursorPayload | null {
+    try {
+        const jsonStr = Buffer.from(cursorStr, 'base64').toString('utf-8');
+        const parsed = JSON.parse(jsonStr);
+
+        if(parsed.timestamp && parsed.id){
+            return parsed;
+        }
+        return null;
+    }catch(error){
+        return null;
+    }
+}
