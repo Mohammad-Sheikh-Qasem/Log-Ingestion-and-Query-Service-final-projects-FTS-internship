d575bbb70fa7fe61cc66119a8580c54a8407a483
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-18 20:00:10 +0300

    add post endpoint logs

--- src/routes/log.routes.ts
+++ src/routes/log.routes.ts
@@ -2,4 +2,11 @@ import { Router } from 'express';
 import { ingestLogsHandler } from '../controllers/log.controller.js';
 import { queryLogsHandler } from '../controllers/query.controller.js';
 import { aggregateLogsHandler } from '../controllers/aggregate.controller.js';
-import { cleanupOldLogs } from '../services/retention.service.js';
+import { cleanupOldLogs } from '../services/index.js';
+
+
+const router = Router();
+
+router.post('/logs', ingestLogsHandler);
+
+export default router;
