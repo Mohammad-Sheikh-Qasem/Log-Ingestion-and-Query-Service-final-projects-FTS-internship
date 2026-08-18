63fdcf4f0fa6d5a7f64c5294f065880476448fa0
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-18 19:54:41 +0300

    add ISO timestamp validation for log queries

--- src/controllers/query.controller.ts
+++ src/controllers/query.controller.ts
@@ -3,3 +3,8 @@ import { pool } from '../db/index.js';
 import { decodeCursor, encodeCursor } from '../utils/cursor.js';
 
 const VALID_LEVELS = ['debug', 'info', 'warn', 'error'];
+
+function isValidIsoTimestamp(val: string): boolean {
+    const d = new Date(val);
+    return !isNaN(d.getTime());
+}
