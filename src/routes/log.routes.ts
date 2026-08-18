a93831f94af2313da9fb9694b480dc3e41d6b1d9
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-18 20:01:50 +0300

    add post endpoint /admin/retention/run

--- src/routes/log.routes.ts
+++ src/routes/log.routes.ts
@@ -11,5 +11,11 @@ router.post('/logs', ingestLogsHandler);
 router.get('/logs', queryLogsHandler);
 router.get('/logs/aggregate', aggregateLogsHandler);
 
+router.post('/admin/retention/run', async (req, res) => {
+    const deletedCount = await cleanupOldLogs();
+    res.status(200).json({ status: 'success', deletedCount });
+});
+
+
 
 export default router;
