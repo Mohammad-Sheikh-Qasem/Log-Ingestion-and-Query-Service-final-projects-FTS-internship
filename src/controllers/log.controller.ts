8b85580860b58c41f4417dbf0fa155b0b28e9c6b
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-18 19:48:22 +0300

    add interface PendingBatch

--- src/controllers/log.controller.ts
+++ src/controllers/log.controller.ts
@@ -1,3 +1,12 @@
 import { Request, Response } from 'express';
 import { LogItemSchema, LogItem } from '../schemas/log.schema.js';
 import { pool } from '../db/index.js';
+
+const TARGET_FLUSH_SIZE = 4000;
+const MAX_CONCURRENT_FLUSHES = 3;
+
+interface PendingBatch {
+    entries: LogItem[];
+    resolve: () => void;
+    reject: (error: unknown) => void;
+}
