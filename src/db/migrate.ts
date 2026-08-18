7b4a4c501c04d17439e1c43bc9bc3c854c1e43cf
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-08-18 18:42:32 +0300

    add create table query

---
+++ src/db/migrate.ts
@@ -0,0 +1,23 @@
+import { pool} from './index.js';
+
+
+export async function runMigration(){
+    const client = await pool.connect();
+
+    try {
+        await client.query('BEGIN');
+
+        await client.query(`
+        CREATE TABLE IF NOT EXISTS logs(
+            id BIGSERIAL PRIMARY KEY,
+            timestamp TIMESTAMPZ NOT NULL,
+            level VARCHAR(10) NOT NULL,
+            service TEXT NOT NULL,
+            attributes JSONB NOT NULL DEFAULT '{}'::jsonb
+        );
+        `);
+
+    }catch(error){
+
+    }
+}
